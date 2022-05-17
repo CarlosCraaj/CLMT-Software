cd19a337f27ca3ca650f63c169694a611ff1af28
Carlos Craaj <carloscraaj@gmail.com>
2022-05-17 01:12:05 -0300

    add/changes rotas

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -17,17 +17,34 @@ public class Aplicacao {
         
         staticFiles.location("/public");
         
+        // LoteService
+        post("/lote/create", (request, response) -> loteService.insert(request, response));
+
+        get("/lote/:id", (request, response) -> loteService.get(request, response));
+
+        post("/lote/update/:id", (request, response) -> loteService.update(request, response));
+
+        get("/lote/delete/:id", (request, response) -> loteService.remove(request, response));
+
+        // UsersService
+
         post("/user/create", (request, response) -> userService.insert(request, response));
 
-        get("/user/get/:username", (request, response) -> userService.get(request, response));
+        get("/user/:id", (request, response) -> userService.get(request, response));
 
-        post("/produto/insert", (request, response) -> productService.insert(request, response));
+        post("/user/update/:id", (request, response) -> userService.update(request, response));
 
-        get("/produto/:id", (request, response) -> productService.get(request, response));
+        get("/user/delete/:id", (request, response) -> userService.remove(request, response));
+
+        // ProductService
+
+        post("/product/create", (request, response) -> productService.insert(request, response));
+
+        get("/product/:id", (request, response) -> productService.get(request, response));
         
-        post("/produto/update/:id", (request, response) -> productService.update(request, response));
+        post("/product/update/:id", (request, response) -> productService.update(request, response));
            
-        get("/produto/delete/:id", (request, response) -> productService.remove(request, response));
+        get("/product/delete/:id", (request, response) -> productService.remove(request, response));
 
     }
 }

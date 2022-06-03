dab7280559df640c818eaec190e68c62acf47402
Carlos Craaj <carloscraaj@gmail.com>
2022-06-02 23:05:15 -0300

    Aplicacao.java

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -19,31 +19,22 @@ public class Aplicacao {
         
         // LoteService
         post("/lote/create", (request, response) -> loteService.insert(request, response));
-
-        get("/lote/:id", (request, response) -> loteService.get(request, response));
-
+        get("/lote/detalhe/:id", (request, response) -> loteService.get(request, response));
         post("/lote/update/:id", (request, response) -> loteService.update(request, response));
-
         get("/lote/delete/:id", (request, response) -> loteService.remove(request, response));
 
         // UsersService
 
         post("/user/create", (request, response) -> userService.insert(request, response));
-
-        get("/user/:id", (request, response) -> userService.get(request, response));
-
+        get("/user/detalhe/:id", (request, response) -> userService.get(request, response));
         post("/user/update/:id", (request, response) -> userService.update(request, response));
-
         get("/user/delete/:id", (request, response) -> userService.remove(request, response));
 
         // ProductService
 
         post("/product/create", (request, response) -> productService.insert(request, response));
-
-        get("/product/:id", (request, response) -> productService.get(request, response));
-        
+        get("/product/detalhe/:id", (request, response) -> productService.get(request, response));
         post("/product/update/:id", (request, response) -> productService.update(request, response));
-           
         get("/product/delete/:id", (request, response) -> productService.remove(request, response));
 
     }

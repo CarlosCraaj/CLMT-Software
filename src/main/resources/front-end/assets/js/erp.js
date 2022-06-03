271e0ec9b370b5cc6d6e3a286eb8730ebe3b0cd4
matheusaugm <matheusmeka01@gmail.com>
2022-06-03 02:20:24 -0300

    layout(erp): changes on erp styling

--- src/main/resources/front-end/assets/js/erp.js
+++ src/main/resources/front-end/assets/js/erp.js
@@ -2,6 +2,8 @@ window.onload = () => {
    localStorage.getItem('USER');
    const userName = localStorage.getItem('USER');
    userName
-      ? (document.querySelector('.userMenuHello').innerText = `Olá,${userName} !`)
+      ? (document.querySelector('.userMenuHello').innerText = `Olá, ${userName} !`)
       : (document.querySelector('.userMenuHello').innerText = 'Olá!');
+
+   doc;
 };
